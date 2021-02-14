c0eb26136705de2f45f68584cb16f4d20e25b39a
ZillKhan <zill277@gmail.com>
2021-02-14 15:04:23 +0500

    Adding all the fucntions

--- stats.c
+++ stats.c
@@ -23,11 +23,10 @@
  */
 
 
-
 #include <stdio.h>
 #include "stats.h"
 
-/* Size of the Data Set */
+
 #define SIZE (40)
 
 void main() {
@@ -37,38 +36,121 @@ void main() {
                               200, 122, 150, 90,   92,  87, 177, 244,
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
-
+  
   int mean,max,min;
-  float median
+  float median;
+  
+  printf("The unsorted Array is \n"); 
+  printf("==================== \n");
+  print_array(test,SIZE); 
+  printf("======================\n");
 
+  array_sort(test,SIZE);
+  printf("The sorted Array from Largest to Smaller is \n"); 
+  printf("==================== \n");
+  print_array(test,SIZE); 
+  printf("======================\n");
+  
+  max = find_max(test,SIZE);
+  min = find_min(test,SIZE);
+  mean = find_mean(test,SIZE);
+  median = find_median(test,SIZE);
+  print_stats(min,max,mean,median);
+  
 }
+
 
 void print_stats(int min, int max, int mean, float median){
   
+  printf("The Array statistics are \n"); 
+  printf("==================== \n");
+  printf("--> Min value is : %i \n",min);
+  printf("==================== \n");
+  printf("--> Max value is : %i \n",max);
+  printf("==================== \n");
+  printf("--> Mean value is : %i \n",mean);
+  printf("==================== \n");
+  printf("--> Median value is : %f \n",median);
+  printf("==================== \n");  
   
 }
 
 void print_array(unsigned char * ptr, int count){
-
+	int i = 0;
+	while (i < count){
+		printf("Array %ith Element is %i \n",i+1,ptr[i]);
+		i++;
+	}
 }
 
 void array_sort(unsigned char * ptr, int count){
+	int i, j, swap = 0;
+
+	for (i=0 ; i<count; i++){
+
+		for(j=0 ; j<count-1 ; j++){
+			if(ptr[j] < ptr[j+1]){
+
+					swap = ptr[j];
+					ptr[j] = ptr[j+1];
+					ptr[j+1] = swap;
+			}
+		}
+	}
+
 	
 }
 
 float find_median (unsigned char ptr[],int count){
+	float med;
 
+	if(count%2 == 0){
+		med = (ptr[(count-1)/2]+ptr[count/2])/2;
+	}
+	else{
+		med = ptr[count/2];
+	}
+	return med;
 }
 
 int find_mean(unsigned char * ptr, int count){
-
+	int i = 0;
+	int sum = 0;
+	while(i<count){
+		sum += ptr[i];
+		i++;
+	}
+	return (sum/count);
 }
 
 int find_max(unsigned char * ptr, int count){
-
+	int i = 0;
+	int max;
+	while (i < count){
+	
+		if (i == 0){
+			max = ptr[i];
+		}
+		if (ptr[i] > max){
+			max = ptr[i];
+		}
+		i++;
+	}
+	return max;
 }
 
 int find_min(unsigned char * ptr, int count){
-
+	int i = 0;
+	int min;
+	while (i < count){
+		if (i == 0){
+			min = ptr[i];
+		}
+		if (ptr[i] < min){
+			min = ptr[i];
+		}
+		i++;
+	}
+	return min;
 }
 
